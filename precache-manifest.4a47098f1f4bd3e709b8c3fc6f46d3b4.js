526f19da68fb2c88706bb9c1a1db51a198ccdbb6
Somnath Paul <Somnath4x@gmail.com>
2020-02-06 18:40:34 +0530

    Updates

---
+++ precache-manifest.4a47098f1f4bd3e709b8c3fc6f46d3b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c401b0d58404f9e5ac0359e660b2a30",
+    "url": "/job-serach/index.html"
+  },
+  {
+    "revision": "dcb7a492e620cb969cc0",
+    "url": "/job-serach/static/css/main.e8d3918d.chunk.css"
+  },
+  {
+    "revision": "228f5707f7a5e47747ef",
+    "url": "/job-serach/static/js/2.d0e35b3a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/job-serach/static/js/2.d0e35b3a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dcb7a492e620cb969cc0",
+    "url": "/job-serach/static/js/main.8c63d8c3.chunk.js"
+  },
+  {
+    "revision": "8bfda166e09edfc9bf9d",
+    "url": "/job-serach/static/js/runtime-main.09bb1836.js"
+  }
+]);
