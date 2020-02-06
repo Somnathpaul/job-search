888df5029efbe5c08f4fefedd735b649cf5dcae5
Somnath Paul <Somnath4x@gmail.com>
2020-02-06 18:43:49 +0530

    Initial commit  Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+
+import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/searchBox/searchBox-component';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      jobs:[],
+      searchField: ''
+    }
+  }
+  // load data when website get loaded
+  componentDidMount(){
+    fetch('positions.json?description=javascript&location=new+york')
+    .then(response => response.json())
+    .then(jsonData => this.setState( {jobs: jsonData} ));
+  }
+
+  // arrow function will automatically bind the function with this
+   handelChange = e => {
+   this.setState({searchField: e.target.value})
+  }
+
+  render(){
+    // destructuring state 
+    const {jobs, searchField} = this.state;
+    const filterJobs = jobs.filter( data => data.title.toLowerCase().includes(searchField.toLowerCase()));
+
+    return (
+      <div className="App">
+        <h1>Job Search</h1>
+        <SearchBox placeholder='full stack' handelChange = {this.handelChange}/>
+        <CardList jobs={filterJobs}></CardList>
+        
+        
+        
+      </div>
+    )
+  }
 }
 
 export default App;
